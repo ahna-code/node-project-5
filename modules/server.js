391521dcb05bffdc02e351aea396311ad5b15862
Agnieszka Korczak <korczak.a.m@gmail.com>
2018-11-20 09:59:49 +0100

    Change handlers.js and server.js

--- modules/server.js
+++ modules/server.js
@@ -11,17 +11,18 @@ function start() {
   response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
  
 
-  switch (request.url) { 
-        case '/':
-        case '/start':
-            handlers.welcome(request, response);
-            break;
-        case '/upload':
-            handlers.upload(request, response);
-            break;
-        case '/show':
-            handlers.show(request, response);
-        break;
+  switch (request.url) {
+case '/':
+case '/start':
+    handlers.welcome(request, response);
+    break;
+case '/upload':
+    handlers.upload(request, response);
+    break;
+case '/show':
+    handlers.show(request, response);
+    break;
+
         default:
             handlers.error(request, response);
     }
